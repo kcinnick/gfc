a67307654d34711dd9adb065b0a2e3c21d7853cf
kcinnick <nicktucker4@gmail.com>
2021-07-04 13:08:36 -0400

    update tests & a bit of logic in preparation for parsing claim reviews

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import ast
 import datetime
 import os
-from collections import defaultdict
 from time import sleep
 
 import requests
 from sqlalchemy import create_engine, func
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError, StatementError
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
 
-from models import Claim, Claimant, claims, claimants
+from models import Claim, Claimant, claims
 
 
 def create_db_session():
@@ -39,78 +39,78 @@ def process_claim(claim, session):
     name = claim.get('claimant', 'Social media users').title()
     if 'social media' in name.lower():
         name = 'Social media users'
-    try:
-        claimant = Claimant(name=name)
-    except KeyError:
-        claimant = Claimant(name=name)
-    session.add(claimant)
-    try:
-        session.commit()
-    except IntegrityError:
-        print(f'Claimant "{claimant.name}" is already in the database.')
-        session.rollback()
-        claimant = session.query(Claimant).where(Claimant.name == claimant.name).first()
 
+    claimant = add_claimant_to_database(session, name)
     try:
         claim_date = datetime.datetime.strptime(claim["claimDate"][:-10], '%Y-%m-%d').date()
     except KeyError:
         claim_date = datetime.datetime.strptime(claim['claimReview'][0]["reviewDate"][:-10], '%Y-%m-%d').date()
 
-    claim = Claim(
-        text=claim["text"],
-        date=claim_date,
-        claimant=claimant
-    )
-    session.add(claim)
-    try:
-        session.commit()
-    except IntegrityError:
-        print(f'Claim "{claim.text}" is already in the database.')
-        session.rollback()
-
+    claim = add_claim_to_database(session, context={
+        'claim_text': claim['text'],
+        'claim_date': claim_date,
+        'claimant': claimant
+    })
     # TODO: parse claim reviews
 
     return claim
 
 
-def search_query(API_KEY):
+def search_query(API_KEY, page_size, query, max_age, language_code='en-US'):
+    """
+
+    :type API_KEY: str, Google Fact Check API key
+    :type page_size: int, number of results to return per page
+    :type query: str, query to be searched
+    :type max_age: int, maximum age of fact check
+    """
     offset = 0
     total_claims = []
-    try:
-        while True:
-            url = (
-                f"https://content-factchecktools.googleapis.com/v1alpha1/claims:search?"
-                f"pageSize=10&"
-                f"query=biden&"
-                f"maxAgeDays=365&"
-                f"offset={offset}&languageCode=en-US&key={API_KEY}"
-            )
-            print('Requesting URL ', url)
-            r = requests.get(url, headers={"x-referer": "https://explorer.apis.google.com"})
-            data = r.json()
+    while True:
+        url = (
+            f"https://content-factchecktools.googleapis.com/v1alpha1/claims:search?"
+            f"pageSize={page_size}&"
+            f"query={query}&"
+            f"maxAgeDays={max_age}&"
+            f"offset={offset}&"
+            f"languageCode={language_code}&"
+            f"key={API_KEY}"
+        )
+        print('Requesting URL ', url)
+        r = requests.get(url, headers={
+            "x-referer": "https://explorer.apis.google.com",
+        })
+        data = r.json()
 
-            next_page_token = data.get("nextPageToken")
-            print(next_page_token)
-            try:
-                claims = data.pop("claims")
-                total_claims.extend(claims)
-            except KeyError:
-                print(data)
-                if data['error']['status'] == 'PERMISSION_DENIED':
-                    sleep(30)
-                    continue
-            offset += 10
-            print(len(total_claims))
-            sleep(5)
-    except Exception as e:
-        print(e)
+        if not data:
+            # results have stopped being returned.
+            # exit the loop and return the results for processing.
+            break
+
+        try:
+            claims = data.pop("claims")
+            total_claims.extend(claims)
+        except KeyError:
+            if data['error']['status'] == 'PERMISSION_DENIED':
+                print('hit permission denied error')
+                sleep(30)
+                continue
+
+        offset += 10
+        if data.get('error'):
+            print('There was an error:')
+            print(data)
+            raise InterruptedError()
+
+        print('Total claims: ', len(total_claims))
+        sleep(5)
 
     return total_claims
 
 
-def main():
+def main(page_size, query, max_age):
     API_KEY = get_api_key()
-    claims = search_query(API_KEY=API_KEY)
+    claims = search_query(API_KEY=API_KEY, page_size=page_size, query=query, max_age=max_age)
     session = create_db_session()
     for claim in claims:
         process_claim(claim, session)
@@ -124,22 +124,111 @@ def source_of_claims(session):
         claims._columns['claimant_id']
     ).all()  # returns tuple of claimant_id, # of instances
 
-    parsed_results = defaultdict(list)
+    parsed_results = {}
     for result in results:
         claimant = session.query(Claimant).get(ident=result[0])
         claims_ = session.query(Claim).where(Claim.claimant_id == claimant.id).all()
         parsed_results[claimant] = dict(claims=claims_, number_of_claims=len(claims_))
 
-    for key, value in parsed_results.items():
-        print('~~~')
-        print(key)
-        print(value)
-
     #  TODO: build claim result stuff
     return parsed_results
 
 
+def search_query_via_toolbox():
+    """
+    The 'content-factchecktools' URL doesn't seem to support the same kind of filters
+    like `recent` or `site` that the are mentioned here: https://toolbox.google.com/factcheck/about#fce-included.
+    This implementation is an attempt to allow programmatic access to them
+    in the same way that regular query searches can be handled.
+    :return:
+    """
+    url = 'https://toolbox.google.com/factcheck/api/search?hl=en&num_results=10&query=list%3Arecent&force=false&offset=0'
+    print('Requesting URL ', url)
+    r = requests.get(url, headers={
+        "x-referer": "https://explorer.apis.google.com",
+    })
+    raw_response = str(r.content).replace(r'\\', '').replace(' "', ' \\"').replace('" ', '\\" ').replace('""', '\"')
+    processed_response = ast.literal_eval(raw_response[11:-1].replace('null', str("None")))[
+        0]  # there are no other items in this list
+
+    # it looks like index 1 is where the actual claim data is held.
+    # index 0 is just a str and index 2 is a list of topics
+    claims_response = processed_response[1]
+    topics = processed_response[2]  # need to find out what this actually is
+
+    return claims_response
+
+
+def add_claimant_to_database(session, name):
+    claimant = Claimant(name=name)
+    session.add(claimant)
+    try:
+        session.commit()
+        print(f'Claimant "{claimant.name}" has been added to the database.')
+    except IntegrityError:
+        print(f'Claimant "{claimant.name}" is already in the database.')
+        session.rollback()
+
+    claimant = session.query(Claimant).where(Claimant.name == claimant.name).first()
+    return claimant
+
+
+def add_claim_to_database(session, context):
+    claim = Claim(
+        text=context['claim_text'],
+        date=context['claim_date'],
+        claimant=context['claimant'],
+    )
+    session.add(claim)
+    try:
+        session.commit()
+        print(f'Claim "{claim.text}" has been added to the database.')
+    except IntegrityError:
+        print(f'Claim "{claim.text}" is already in the database.')
+        session.rollback()
+    except StatementError:
+        session.rollback()
+        year, day, month = context['claim_date'].split('-')
+        context['claim_date'] = datetime.date(int(year), int(day), int(month))
+        claim = Claim(
+            text=context['claim_text'],
+            date=context['claim_date'],
+            claimant=context['claimant'],
+        )
+        session.add(claim)
+
+    return claim
+
+
+def process_claim_from_toolbox(claim_toolbox_object, session):
+    claim_data = claim_toolbox_object[0]
+    claim_text = claim_data.pop(0)
+    name = claim_data.pop(0)[0]
+    claim_date = claim_data.pop(0)  # this is a UNIX timestamp of the claim date
+    try:
+        claim_date = datetime.datetime.utcfromtimestamp(claim_date).strftime("%Y-%m-%d")
+    except TypeError:
+        claim_date = None
+    print(claim_date)
+    claim_review_source_data = claim_data.pop(0)[0][0]  # TODO: address later when we actually handle claim review stuff
+    if 'social media' in name.lower():
+        name = 'Social media users'
+    claimant = add_claimant_to_database(session, name)
+    claim = add_claim_to_database(
+        session, context={'claim_text': claim_text,
+                          'claim_date': claim_date,
+                          'claimant': claimant
+                          })
+
+    # TODO: parse claim reviews
+
+    return
+
+
 if __name__ == "__main__":
-    main()
+    # main(page_size=10, query='biden', max_age=14)
     session = create_db_session()
-    source_of_claims(session=session)
+    # source_of_claims(session=session)
+    claims_response = search_query_via_toolbox()
+    for claim in claims_response:
+        process_claim_from_toolbox(claim, session)
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,38 +1,144 @@
 import datetime
 import os
+import random
+import unittest
 
-from main import get_api_key, search_query, process_claim, create_db_session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from sqlalchemy.pool import NullPool
 
-
-def test_get_api_key():
-    API_KEY = get_api_key()
-    assert API_KEY == os.getenv("API_KEY")
-
-
-def test_search_query():
-    API_KEY = os.getenv("API_KEY")
-    claims = search_query(API_KEY)
-    assert type(claims) == list
-    assert len(claims) == 10
+from main import get_api_key, search_query, process_claim, search_query_via_toolbox, create_db_session, \
+    process_claim_from_toolbox, source_of_claims
+from models import Claimant, Claim
 
 
-def test_process_claims():
-    example_claim = {
-        'text': 'An image shows President Joe Biden sleeping at G-7 summit',
-        'claimant': 'Social media users',
-        'claimDate': '2021-06-16T00:00:00Z',
-        'claimReview': [
-            {'publisher': {'name': 'USA Today', 'site': 'usatoday.com'},
-             'url': 'https://www.usatoday.com/story/news/factcheck/2021/06/20/fact-check-'
-                    'photo-biden-sleeping-g-7-summit-altered/7729133002/',
-             'title': 'Fact check: Image claiming to show Joe Biden sleeping at G-7 ...',
-             'reviewDate': '2021-06-20T21:41:07Z',
-             'textualRating': 'Altered',
-             'languageCode': 'en'}
-        ]
-    }
-    session = create_db_session()
-    processed_claim = process_claim(example_claim, session)
-    assert processed_claim.text == 'An image shows President Joe Biden sleeping at G-7 summit'
-    assert processed_claim.date == datetime.date(2021, 6, 16)
-    assert processed_claim.claimant.name == 'Social media users'
+class TestSearchQuery(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine(
+            'sqlite:///gfc.db', echo=False,
+            poolclass=NullPool,
+        )
+        Base = declarative_base()
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def test_get_api_key(self):
+        API_KEY = get_api_key()
+        assert API_KEY == os.getenv("API_KEY")
+
+    def test_search_query(self):
+        API_KEY = os.getenv("API_KEY")
+        query = 'Biden'
+
+        claims = search_query(API_KEY, page_size=10, query=query, max_age=7)
+        assert type(claims) == list
+        assert len(claims) >= 1
+
+
+class TestProcessClaims(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine(
+            'sqlite:///gfc.db', echo=False,
+            poolclass=NullPool,
+        )
+        Base = declarative_base()
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+        self.session.add(Claimant(name='Cristian'))
+        self.session.commit()
+
+    def test_process_claims(self):
+        example_claim = {
+            'text': 'An image shows President Joe Biden sleeping at G-7 summit',
+            'claimant': 'Social media users',
+            'claimDate': '2021-06-16T00:00:00Z',
+            'claimReview': [
+                {'publisher': {'name': 'USA Today', 'site': 'usatoday.com'},
+                 'url': 'https://www.usatoday.com/story/news/factcheck/2021/06/20/fact-check-'
+                        'photo-biden-sleeping-g-7-summit-altered/7729133002/',
+                 'title': 'Fact check: Image claiming to show Joe Biden sleeping at G-7 ...',
+                 'reviewDate': '2021-06-20T21:41:07Z',
+                 'textualRating': 'Altered',
+                 'languageCode': 'en'}
+            ]
+        }
+        processed_claim = process_claim(example_claim, self.session)
+        assert processed_claim.text == 'An image shows President Joe Biden sleeping at G-7 summit'
+        assert processed_claim.date == datetime.date(2021, 6, 16)
+        assert processed_claim.claimant.name == 'Social media users'
+
+    def test_add_claim_to_database(self):
+        return
+
+    def test_create_db_session(self):
+        create_db_session()
+
+    def tearDown(self):
+        user = self.session.query(Claimant).where(Claimant.name == 'Cristian')
+        user.delete()
+        self.session.commit()
+        pass
+
+
+class TestToolboxQueries(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine(
+            'sqlite:///gfc.db', echo=False,
+            poolclass=NullPool,
+        )
+        Base = declarative_base()
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+
+    def test_search_query_via_toolbox(self):
+        claims_response = search_query_via_toolbox()
+        assert len(claims_response) == 10
+
+    def test_process_claim_from_toolbox(self):
+        for claim in search_query_via_toolbox():
+            process_claim_from_toolbox(claim, self.session)
+
+    def tearDown(self) -> None:
+        objects = Claim.__table__.delete()
+        self.session.execute(objects)
+        self.session.commit()
+
+
+class TestSourceOfClaims(unittest.TestCase):
+    def setUp(self):
+        engine = create_engine(
+            'sqlite:///gfc.db', echo=False,
+            poolclass=NullPool,
+        )
+        Base = declarative_base()
+        Base.metadata.create_all(engine)
+
+        Session = sessionmaker(bind=engine)
+        self.session = Session()
+        claimant = Claimant(name='this is a claimant', id=9)
+        claim = Claim(text='this is a claim', claimant_id=claimant.id)
+        self.session.add(claim)
+        self.session.add(claimant)
+        another_claim = Claim(text='this is another claim', claimant_id=claimant.id)
+        self.session.add(another_claim)
+        self.session.commit()
+
+    def test_source_of_claims(self):
+        parsed_results = source_of_claims(self.session)
+        self.assertIn('this is a claimant', [i.name for i in parsed_results])
+        self.assertIn('this is a claimant', [i.name for i in parsed_results])
+
+    def tearDown(self) -> None:
+        objects = Claim.__table__.delete()
+        self.session.execute(objects)
+        self.session.commit()
+        objects = Claimant.__table__.delete()
+        self.session.execute(objects)
+        self.session.commit()
